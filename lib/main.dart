ee717475a6aad282a070ed628178796bcfaa84b5
Toretay Abdymazhit <abdymvzhit@gmail.com>
2025-11-28 19:25:19 +0500

    create HomePage widget to display list of chats

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:messenger_app/home_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'The Messenger',
-      home: const Scaffold(),
+      home: HomePage(),
     );
   }
 }
