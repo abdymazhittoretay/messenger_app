5dcfb448b049123738812a4222806aa0e8362939
Toretay Abdymazhit <abdymvzhit@gmail.com>
2025-11-28 19:22:02 +0500

    default set up for main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'The Messenger',
+      home: const Scaffold(),
     );
   }
 }
